7c4d1569eb8a97d1bd87b58a945cf26306693896
benghwee <swiftx888@gmail.com>
2023-12-28 06:48:35 +0800

    Add files via upload

---
+++ src/test/java/factory/UnitTestSuite.java
@@ -0,0 +1,15 @@
+package factory;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   UnitTest02.class,
+   UnitTest01.class
+})
+
+public class UnitTestSuite {
+
+}
